1a992db0ac80d312652a5627625fbb41b1b95c5e
maximilian.rahn <maximilian.rahn@onelogic.de>
2021-08-16 18:21:12 +0200

    adding dockerfile

--- hooks/post_gen_project.bat
+++ hooks/post_gen_project.bat
@@ -1,11 +1,5 @@
 git init
 git add .
 git commit -m "project setup commit"
-git checkout -b "dev"
 
 
-{% if cookiecutter.conda_env_install == "yes" %}
-
-conda env create -f ../{{cookiecutter.project_slug}}/envs/environment.yml -n {{cookiecutter.conda_env_name}}
-
-{% endif %}
