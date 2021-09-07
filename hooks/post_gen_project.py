8e4d7985266638d5c0fb609c5dd786b1e0a7383f
maxthemillion <max.rahn@tum.de>
2021-09-07 20:22:11 +0200

    fix git init

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -13,4 +13,7 @@ if __name__ == "__main__":
         os.system("conda run -n {{ cookiecutter.conda_env_name }} deon --output ETHICS.md")
 
     if "{{ cookiecutter.init_git_repo }}" == "yes":
-        os.system("git init & git commit -a -m 'initial commit'")
+        os.system("git init")
+        os.system("git add .")
+        os.system("git commit -a -m 'initial commit'")
+        os.system("git checkout -b dev")
