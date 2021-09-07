88eec23bb1b7edd6fbc5ac9a00a90845b947e47e
maxthemillion <max.rahn@tum.de>
2021-09-07 10:06:26 +0200

    added pytest test structure

---
+++ {{cookiecutter.project_slug}}/tests/test_utils.py
@@ -0,0 +1,14 @@
+import pytest
+from utils.utils import MyClass
+
+
+@pytest.fixture
+def instance():
+    return MyClass()
+
+class TestMyClass:
+    def setUp(self):
+        pass
+
+    def test_my_method(self, instance):
+        assert instance.my_method() == True
