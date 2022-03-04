f9009bb6248947cf3077bdf0eeac6f72a34e4466
Max Rahn <maximilian.rahn@onelogic.de>
2022-03-04 09:14:22 +0100

    vim

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,17 +7,18 @@ if __name__ == "__main__":
     if "{{ cookiecutter.create_conda_env }}" == "yes":
         EDITOR = os.environ.get('EDITOR','vim')
 
-        initial_message = b"edit" # if you want to set up the file somehow
+        with open("./{{cookiecutter.project_slug}}/environment.yml", 'rb') as f: 
+            initial_message = f.read()
 
         with tempfile.NamedTemporaryFile(suffix=".tmp") as tf:
             tf.write(initial_message)
             tf.flush()
             call([EDITOR, tf.name])
 
-        # do the parsing with `tf` using regular File operations.
-        # for instance:
-        tf.seek(0)
-        edited_message = tf.read()
+            # do the parsing with `tf` using regular File operations.
+            # for instance:
+            tf.seek(0)
+            edited_message = tf.read()
 
         os.system(
             "conda env create "
