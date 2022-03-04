ce7804c8c0ac37c7873cda9f009801639b29c926
Max Rahn <maximilian.rahn@onelogic.de>
2022-03-04 09:06:35 +0100

    editing vim file

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,9 +1,24 @@
 
-import os
+import sys, tempfile, os
+from subprocess import call
 
 if __name__ == "__main__":
     
     if "{{ cookiecutter.create_conda_env }}" == "yes":
+        EDITOR = os.environ.get('EDITOR','vim')
+
+        initial_message = b"edit" # if you want to set up the file somehow
+
+        with tempfile.NamedTemporaryFile(suffix=".tmp") as tf:
+            tf.write(initial_message)
+            tf.flush()
+            call([EDITOR, tf.name])
+
+        # do the parsing with `tf` using regular File operations.
+        # for instance:
+        tf.seek(0)
+        edited_message = tf.read()
+
         os.system(
             "conda env create "
             "--file environment.yml "
